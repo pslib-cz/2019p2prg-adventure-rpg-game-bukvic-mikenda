4da03d88703425efc8da4c3fb8d21ddd198f43d2
Filip Bukvic <u18.405@pslib.cloud>
2020-05-13 13:39:27 +0200

    Added SessionStorage

---
+++ RPG/RPG/Model/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RPG.Model
+{
+    public class User
+    {
+        public User(string first = "", string last = "")
+        {
+            FirstName = first;
+            LastName = last;
+        }
+
+        public Guid UserId { get; set; }
+        public string FullName => $"{FirstName} {LastName}";
+
+        private string FirstName;
+        private string LastName;
+    }
+}
